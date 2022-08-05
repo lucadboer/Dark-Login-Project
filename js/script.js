846f3a7cf72405dc800631584325172094a371f2
Luca Destefano Boer <lucahesper@hotmail.com>
2022-08-04 23:09:38 -0300

    up files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const btn_register = document.querySelector('.btn-register')
-
-btn_register.addEventListener('click', load_page)
-
-function load_page(event) {
-    event.preventDefault();
-    window.location = 'pages/registration.html'
-}
